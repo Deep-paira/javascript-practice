03a1d04b37b1ba24c439943a6e2448e251e6d63c
deep paira <deeppaira45@gmail.com>
2025-08-02 17:04:05 +0530

    update

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -3,7 +3,7 @@
 //Types of array declaration..
 
 const myArr = [0, 1, 2, 3, 4, 5]
-const myHeors = ["spiderman", "Thor"]
+const myHeros = ["spiderman", "Thor"]
 
 const myArr2 = new Array(1, 2, 3, 4)
 //console.log(myArr[3]);
